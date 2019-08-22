63b1b2e66cd91cfdc8b8e074b872e44fded4db2c
Tishoy <chuang.han@mobvista.com>
2019-08-22 11:41:15 +0800

    增加 珠宝类型 获取珠宝分值 方法

--- RichMan/src/Jewels.ts
+++ RichMan/src/Jewels.ts
@@ -4,20 +4,17 @@ class Jewels extends egret.Sprite {
     private speed: number;
     private score: number;
     private type: number;
-    private type_mc;
-    private coin_mc;
-    private coin_tw: egret.Tween;
-    private shoe_shaped_mc;
-    private shoe_shaped_tw: egret.Tween;
-    private chest_mc;
-    private chest_tw: egret.Tween;
-    private bomb_mc;
-    private bomb_tw: egret.Tween;
+    private type_mc: egret.MovieClip;
+    private coin_mc: egret.MovieClip;
+    private shoe_shaped_mc: egret.MovieClip;
+    private chest_mc: egret.MovieClip;
+    private bomb_mc: egret.MovieClip;
+
     constructor(type) {
         super();
         //分值和速度根据类型确定
         this.speed = 10 * type;
-        this.score = 10 * type;
+        this.score = JewelsType.getScoreByJewelsType(type);
         this.type = type;
 
         this.init();
@@ -26,7 +23,7 @@ class Jewels extends egret.Sprite {
 
 
         if (this.type == JewelsType.COIN) {
-            this.coin_mc = this.buildTypeMc("coin_json","coin_png","coin");
+            this.coin_mc = this.buildTypeMc("coin_json", "coin_png", "coin");
             this.coin_mc.anchorOffsetX = this.coin_mc.width;
             this.coin_mc.anchorOffsetY = this.coin_mc.height;
             this.coin_mc.play(-1);
@@ -34,14 +31,14 @@ class Jewels extends egret.Sprite {
             // this.coin_tw.to({ rotation: 200 }, 2000);
             this.addChild(this.coin_mc);
         } else if (this.type == JewelsType.SHOE_SHAPED) {
-            this.shoe_shaped_mc = this.buildTypeMc("shoe-shaped_json","shoe-shaped_png","shoe-shaped");
+            this.shoe_shaped_mc = this.buildTypeMc("shoe-shaped_json", "shoe-shaped_png", "shoe-shaped");
             this.shoe_shaped_mc.anchorOffsetX = this.shoe_shaped_mc.width / 2;
             this.shoe_shaped_mc.anchorOffsetY = this.shoe_shaped_mc.height / 2;
             this.addChild(this.shoe_shaped_mc);
             // this.shoe_shaped_tw = egret.Tween.get(this.shoe_shaped,{loop:true});
             // this.shoe_shaped_tw.to({rotation:360},3000);
         } else if (this.type == JewelsType.CHEST) {
-            this.chest_mc = this.buildTypeMc("chest_json","chest_png","chest");
+            this.chest_mc = this.buildTypeMc("chest_json", "chest_png", "chest");
             this.chest_mc.scaleX = 0.5;
             this.chest_mc.scaleY = 0.5;
             this.chest_mc.anchorOffsetX = this.chest_mc.width / 2;
@@ -49,8 +46,8 @@ class Jewels extends egret.Sprite {
             this.addChild(this.chest_mc);
             // this.chest_tw = egret.Tween.get(this.shoe_shaped,{loop:true});
             // this.chest_tw.to({y:1000,rotation:360},3000);
-        }else{
-            this.bomb_mc = this.buildTypeMc("bomb_json","bomb_png","bomb");
+        } else {
+            this.bomb_mc = this.buildTypeMc("bomb_json", "bomb_png", "bomb");
             this.bomb_mc.anchorOffsetX = this.bomb_mc.width / 2;
             this.bomb_mc.anchorOffsetY = this.bomb_mc.height / 2;
             this.addChild(this.bomb_mc);
@@ -59,11 +56,11 @@ class Jewels extends egret.Sprite {
         egret.Tween.get(this, { loop: true }).to({ rotation: 360 }, 3000)
     }
 
-    private buildTypeMc(type_json,type_data,type_mc_data){
-            let type_json_res = RES.getRes(type_json);
-            let type_data_res = RES.getRes(type_data);
-            let typeFactory = new egret.MovieClipDataFactory(type_json_res, type_data_res);
-            this.type_mc = new egret.MovieClip(typeFactory.generateMovieClipData(type_mc_data));
-            return this.type_mc;
+    private buildTypeMc(type_json, type_data, type_mc_data) {
+        let type_json_res = RES.getRes(type_json);
+        let type_data_res = RES.getRes(type_data);
+        let typeFactory = new egret.MovieClipDataFactory(type_json_res, type_data_res);
+        this.type_mc = new egret.MovieClip(typeFactory.generateMovieClipData(type_mc_data));
+        return this.type_mc;
     }
 }
--- RichMan/src/enum/JewelsType.ts
+++ RichMan/src/enum/JewelsType.ts
@@ -1,11 +1,24 @@
 module JewelsType {
+
+    export var JewelsScore = [];
     //炸弹
     export const BOMB = 0;
+    JewelsScore[BOMB] = -1;
     //金币
     export const COIN = 1;
+    JewelsScore[COIN] = 10;
     //元宝
     export const SHOE_SHAPED = 2;
+    JewelsScore[SHOE_SHAPED] = 10;
     //宝箱
     export const CHEST = 3;
-    
+    JewelsScore[CHEST] = 10;
+    //金条
+    export const GOLD_BAR = 4;
+    JewelsScore[GOLD_BAR] = 10;
+
+    export function getScoreByJewelsType(type) {
+        return JewelsScore[type];
+    }
+
 }
